b0e81349e310af89e1941e1bb9f0ef773e3b7201
Lu Kangping <kplu@thoughtworks.com>
2013-11-24 03:14:09 +0800

    2 select control

--- js/controllers/header_controller.js
+++ js/controllers/header_controller.js
@@ -1,8 +1,32 @@
 define(['app/geo_app'], function(geoApp){
 	
-	
 	geoApp.controller('HeaderController', function HeaderController($scope){
-		$scope.name="roger";
+		$scope.categories=[
+			{'value': 'category1'},
+			{'value': 'category2'}
+		];
+		$scope.category=$scope.categories[0];
+		
+		$scope.topics=[
+			{'value': 'topic1'},
+			{'value': 'topic2'}
+		]
+		$scope.topic=$scope.topics[0];
+		
+		$scope.$watch("category", function(){
+			if($scope.category==$scope.categories[1]){
+				$scope.topics=[
+					{'value': 'topic3'},
+					{'value': 'topic4'}
+				]
+				$scope.topic=$scope.topics[0];
+			}
+		});
+		
+		$scope.query=function(){
+			console.log(Map);
+			// Map.addGrid();
+		}
 	});
 	
 });
